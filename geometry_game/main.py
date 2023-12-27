d3ea46e4e52a10ed71aa959ae947f44915abab23
Lu <77848575+Rojanex@users.noreply.github.com>
2023-12-27 14:49:22 -0500

    test turtle

---
+++ geometry_game/main.py
@@ -0,0 +1,21 @@
+
+import turtle
+
+window = turtle.Screen()
+
+myturtle = turtle.Turtle()
+
+myturtle.showturtle()
+myturtle.speed(1)
+myturtle.penup()
+myturtle.goto(50,75)
+
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+
+turtle.done()
